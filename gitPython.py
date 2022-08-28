a0813afce84fabee11d3f929f9a544e0ba9a7bf9
Keerthith-31 <112301027+Keerthith-31@users.noreply.github.com>
2022-08-28 10:49:52 +0530

    Update gitPython.py

--- gitPython.py
+++ gitPython.py
@@ -1,6 +1,6 @@
 import git
 
-repo_clone_url = "https://github.com/Keerthith-31/PythonProgrammes.py.git"
+repo_clone_url = "https://github.com/Keerthith-31/PythonProgrammes.git"
 test_branch = "checkout"
 checkout = repo.git.checkout('test_branch')
 
